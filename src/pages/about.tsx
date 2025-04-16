90218abfa2cab92c1e36a576b31a59165aa82d6f
Arunabha-NOTE <arunabham2017@gmail.com>
2025-04-16 23:38:33 +0530

    changed colour of text to white in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,12 +7,12 @@ export default function AboutPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>About</h1>
-          <p className="text-gray-700 mt-4">
+          <p className=" mt-4">
             Welcome to the Deepfake Detection App! This website is designed to help users detect 
             whether a video is real or fake using advanced AI models. Simply upload an MP4 video, 
             and our system will analyze it to provide a prediction along with confidence levels.
           </p>
-          <p className="text-gray-700 mt-4">
+          <p className=" mt-4">
             Our mission is to provide a reliable and easy-to-use tool for identifying deepfake content, 
             empowering users to make informed decisions in a world where digital media is increasingly manipulated.
           </p>
